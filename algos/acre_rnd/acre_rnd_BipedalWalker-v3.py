d73be12febf75e5f51fbbb6a42885af18d3749e5
Thanasis Kapoutsis <athakapo@iti.gr>
2022-06-03 21:22:38 +0300

    --

--- algos/acre_rnd/acre_rnd_BipedalWalker-v3.py
+++ algos/acre_rnd/acre_rnd_BipedalWalker-v3.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='Swimmer-v2')
+    parser.add_argument('--env', type=str, default='BipedalWalker-v3')
     parser.add_argument('--reward_type', type=str, default=None)  # None
     parser.add_argument('--hid', type=int, default=256)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
-    parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--epochs', type=int, default=80)
-    parser.add_argument('--beta', type=float, default=1000)
+    parser.add_argument('--seed', '-s', type=int, default=1)
+    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--beta', type=float, default=0.0007)
     parser.add_argument('--RNDoutput_size', type=int, default=64)
     parser.add_argument('--estimate_rnd_every', type=int, default=1)
     parser.add_argument('--exp_name', type=str, default='acre_rnd')
@@ -37,10 +37,9 @@ if __name__ == '__main__':
         logger_tb_args['aggregate_stats'] = args.aggregate_stats
 
     torch.set_num_threads(torch.get_num_threads())
+
     acre_rnd(lambda: gym.make(args.env), actor_critic=core.MLPActorCritic,
              ac_kwargs=dict(hidden_sizes=[args.hid] * args.l), reward_type=args.reward_type,
              gamma=args.gamma, seed=args.seed, epochs=args.epochs, beta=args.beta,
              estimate_rnd_every=args.estimate_rnd_every, RNDoutput_size=args.RNDoutput_size,
              logger_kwargs=logger_kwargs, logger_tb_args=logger_tb_args)
-
-
